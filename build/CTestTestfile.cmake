50bd16440a474602e55594888575816ad6b487c5
dhruvbatra2 <dhruvbatra2@Grovyle.myguest.virtualbox.org>
2023-09-27 12:48:47 -0400

    initial

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,11 @@
+# CMake generated Testfile for 
+# Source directory: /home/dhruvbatra2/catkin_ws/src
+# Build directory: /home/dhruvbatra2/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("beginner_tutorials")
+subdirs("rqt_mypkg")
+subdirs("helloworld")
+subdirs("usb_cam")
---
+++ build/helloworld/firmware/CMakeFiles/hello.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/dhruvbatra2/catkin_ws/devel/share/helloworld/hello.elf"
+  "/home/dhruvbatra2/catkin_ws/devel/share/helloworld/hello.pdb"
+  "CMakeFiles/hello.dir/home/dhruvbatra2/catkin_ws/build/helloworld/ros_lib/time.cpp.obj"
+  "CMakeFiles/hello.dir/scontrol.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
